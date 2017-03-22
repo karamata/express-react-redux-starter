84f82b5b46d06fca49a323f223cade824533eaec
AAVN\tphu <phu.tran@axonactive.com>
2017-03-22 16:09:40 +0700

    init project

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const isUseHTTPs = false;
+
+function resolveURL(url) {
+    var isWin = !!process.platform.match(/^win/);
+    if (!isWin) return url;
+    return url.replace(/\//g, '\\');
+}
+
+var path = require('path');
+
+const options = {
+    key: fs.readFileSync(path.join(__dirname, resolveURL('fake-keys/privatekey.pem'))),
+    cert: fs.readFileSync(path.join(__dirname, resolveURL('fake-keys/certificate.pem')))
+};
+
+const server = require(isUseHTTPs ? 'https' : 'http');
+
+if (isUseHTTPs) {
+    httpServer = server.createServer(options, app);
+} else {
+    httpServer = server.createServer(app);
+}
 
 app.use(express.static('./'));
 app.use(express.static('dist'));
@@ -10,6 +33,22 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+require('./signaling-server.js')(httpServer, function(socket) {
+    try {
+        var params = socket.handshake.query;
+
+        if (!params.socketCustomEvent) {
+            params.socketCustomEvent = 'custom-message';
+        }
+
+        socket.on(params.socketCustomEvent, function(message) {
+            try {
+                socket.broadcast.emit(params.socketCustomEvent, message);
+            } catch (e) {}
+        });
+    } catch (e) {}
+});
+
+httpServer.listen(port, () => {
   console.log('app listening on', port);
 });
